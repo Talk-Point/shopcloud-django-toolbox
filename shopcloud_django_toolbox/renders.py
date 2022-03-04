d55c14f38bc0e9f80488f2ea6a977b716abfaa1a
Konstantin.Stoldt <konstantin.stoldt@talk-point.de>
2022-03-04 09:08:26 +0100

    renderes: add renderer HTMLRenderer BinaryFileRenderer

--- shopcloud_django_toolbox/renders.py
+++ shopcloud_django_toolbox/renders.py
@@ -10,6 +10,14 @@ class PlainTextRenderer(renderers.BaseRenderer):
         return force_str(data, encoding=self.charset)
 
 
+class HTMLRenderer(renderers.BaseRenderer):
+    media_type = 'text/html'
+    format = 'txt'
+
+    def render(self, data, media_type=None, renderer_context=None):
+        return force_str(data, encoding=self.charset)
+
+
 class XMLRenderer(renderers.BaseRenderer):
     media_type = 'application/xml'
     format = 'xml'
@@ -21,3 +29,13 @@ class XMLRenderer(renderers.BaseRenderer):
             renderer_context=None
     ):
         return force_str(data, encoding=self.charset)
+
+
+class BinaryFileRenderer(renderers.BaseRenderer):
+    media_type = 'application/octet-stream'
+    format = None
+    charset = None
+    render_style = 'binary'
+
+    def render(self, data, media_type=None, renderer_context=None):
+        return data
